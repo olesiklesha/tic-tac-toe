8a3f5d5c026eb703b2d948952dc4b180a6d59b9e
olesiklesha <olesik736@gmail.com>
2021-08-30 19:21:09 +0300

    feat: compltete task

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,85 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.currPlayer = "x";
+    this.field = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null]
+    ];
+    this.counter = 0;
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currPlayer;
+  }
+
+  togglePlayer() {
+    return this.currPlayer === "x" ? "o" : "x";
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.field[rowIndex][columnIndex] === null) {
+      this.field[rowIndex][columnIndex] = this.currPlayer;
+      this.currPlayer = this.togglePlayer();
+      this.counter++;
+    }
+  }
+
+  isWin() {
+    if (
+      this.field[0][0] === this.field[1][1] &&
+      this.field[0][0] === this.field[2][2]
+    ) {
+      return this.field[0][0];
     }
 
-    getCurrentPlayerSymbol() {
-
+    if (
+      this.field[0][2] === this.field[1][1] &&
+      this.field[0][2] === this.field[2][0]
+    ) {
+      return this.field[0][2];
     }
 
-    nextTurn(rowIndex, columnIndex) {
-
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.field[i][0] === this.field[i][1] &&
+        this.field[i][0] === this.field[i][2]
+      ) {
+        return this.field[i][0];
+      }
     }
 
-    isFinished() {
-
+    for (let i = 0; i < 3; i++) {
+      if (
+        this.field[0][i] === this.field[1][i] &&
+        this.field[0][i] === this.field[2][i]
+      ) {
+        return this.field[0][i];
+      }
     }
+    return null;
+  }
 
-    getWinner() {
+  isFinished() {
+    return this.isDraw() || !!this.getWinner();
+  }
 
-    }
+  getWinner() {
+    this.winner = this.isWin();
+    return this.winner;
+  }
 
-    noMoreTurns() {
+  noMoreTurns() {
+    return this.counter >= 9 ? true : false;
+  }
 
-    }
+  isDraw() {
+    return this.noMoreTurns() && !this.getWinner();
+  }
 
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
